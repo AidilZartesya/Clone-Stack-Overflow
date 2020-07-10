eec8640d04a8759706c1a9601ce3e731b6880841
BoyPurba <boypoerbah@gmail.com>
2020-07-10 16:52:53 +0700

    form jawaban ++

--- app/Http/Controllers/JawabanController.php
+++ app/Http/Controllers/JawabanController.php
@@ -9,13 +9,12 @@ use App\Models\PertanyaanModel;
 use App\Models\JawabanModel;
 use App\Models\UserModel;
 
-class JawabanControler extends Controller
+class JawabanController extends Controller
 {
     public function index($id)
     {	
         $pertanyaan = PertanyaanModel::find($id);
         $jawaban = JawabanModel::all();
-        dd("10");
     	return view('jawaban.form_jawaban', compact('pertanyaan, jawaban'));
     }
 }
